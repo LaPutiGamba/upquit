73971b102916c6a561da98874608ea0efed3a248
Arià Aragón <arian.aragonferriz@gmail.com>
2026-03-29 20:42:09 +0200

    feat: initialize backend

---
+++ apps/backend/drizzle.config.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  out: "./src/shared/infrastructure/migrations",
+  schema: "./src/modules/*/infrastructure/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!
+  }
+});
